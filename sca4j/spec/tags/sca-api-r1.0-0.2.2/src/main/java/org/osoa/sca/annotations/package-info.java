5db517bf86b5543fb8a25082250e26904a83c494
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2009-08-29 11:48:00 +0000

    [maven-scm] copy for tag sca-api-r1.0-0.2.2

---
+++ sca4j/spec/tags/sca-api-r1.0-0.2.2/src/main/java/org/osoa/sca/annotations/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * SCA4J
+ * Copyright (c) 2008-2012 Service Symphony Limited
+ *
+ * This proprietary software may be used only in connection with the SCA4J license
+ * (the ?License?), a copy of which is included in the software or may be obtained 
+ * at: http://www.servicesymphony.com/licenses/license.html.
+ *
+ * Software distributed under the License is distributed on an as is basis, without 
+ * warranties or conditions of any kind.  See the License for the specific language 
+ * governing permissions and limitations of use of the software. This software is 
+ * distributed in conjunction with other software licensed under different terms. 
+ * See the separate licenses for those programs included in the distribution for the 
+ * permitted and restricted uses of such software.
+ *
+ */
+/**
+ * SCA Common Annotations.
+ * <p/>
+ * Annotations that can be applied to Java code to provide additional meta-data about the SCA component type
+ * or configuration information for the implementation container. These are common annotations that
+ * are intended to provide consistent behaviour across different SCA implementation types but you should consult
+ * the documentation on each implementation type for a definition of how the annotation will be interpreted. 
+ */
+package org.osoa.sca.annotations;
