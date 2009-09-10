90d03f90d18c33d5e542b8c124eda53f3f3107a6
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2009-09-10 11:58:21 +0000

    Runtime booting up

---
+++ sca4j/tests/trunk/test-generic-runtime/src/test/java/org/sca4j/gereric/runtime/test/GenericRuntimeTest.java
@@ -0,0 +1,35 @@
+/*
+ * SCA4J
+ * Copyright (c) 2008-2012 Service Symphony Limited
+ *
+ * This proprietary software may be used only in connection with the SCA4J license
+ * (the ?License?), a copy of which is included in the software or may be obtained 
+ * at: http://www.servicesymphony.com/licenses/license.html.
+ *
+ * Software distributed under the License is distributed on an as is basis, without 
+ * warranties or conditions of any kind.  See the License for the specific language 
+ * governing permissions and limitations of use of the software. This software is 
+ * distributed in conjunction with other software licensed under different terms. 
+ * See the separate licenses for those programs included in the distribution for the 
+ * permitted and restricted uses of such software.
+ *
+ */
+package org.sca4j.gereric.runtime.test;
+
+import java.io.IOException;
+
+import org.sca4j.host.runtime.InitializationException;
+import org.sca4j.runtime.generic.junit.AbstractScaTest;
+
+public class GenericRuntimeTest extends AbstractScaTest {
+    
+    public GenericRuntimeTest() throws IOException, InitializationException {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void test() throws Exception {
+        
+    }
+
+}
