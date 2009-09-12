d14a32e3a4df87d2a5c27e03bd3de220aa9e01d8
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2009-09-12 17:53:06 +0000

    Removed println

--- sca4j/tests/trunk/test-generic-runtime/src/test/java/org/sca4j/gereric/runtime/test/EmployeeServiceTest.java
+++ sca4j/tests/trunk/test-generic-runtime/src/test/java/org/sca4j/gereric/runtime/test/EmployeeServiceTest.java
@@ -36,7 +36,6 @@ public class EmployeeServiceTest extends AbstractScaTest {
 
     public void test() throws Exception {
         EmployeeService employeeService = getServiceProxy(EmployeeService.class, "employeeService");
-        System.err.println(employeeService.getClass());
         String id = employeeService.createEmployee("Meeraj Kunnumpurath");
         assertEquals("Meeraj Kunnumpurath", employeeService.findName(id));
     }
