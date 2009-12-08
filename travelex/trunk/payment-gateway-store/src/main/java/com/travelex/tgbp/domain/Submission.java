ca217a937b0e39851f6a8632d407fee17d74a6d8
kmohan <kmohan@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2009-12-08 16:36:06 +0000

    Domain object initial structure.

---
+++ travelex/trunk/payment-gateway-store/src/main/java/com/travelex/tgbp/domain/Submission.java
@@ -0,0 +1,19 @@
+package com.travelex.tgbp.domain;
+
+/**
+ * Represents file submission entity.
+ */
+public class Submission {
+
+    private Long id;
+
+    /**
+     * Returns entity identifier.
+     *
+     * @return the entity id
+     */
+    public Long getId() {
+        return id;
+    }
+
+}
