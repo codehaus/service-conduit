ad4059082a59fb6db5f784b26e7f0c7b24996bf3
kmohan <kmohan@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2009-12-22 17:15:17 +0000

    Adding something

--- travelex/trunk/payment-gateway-schema-model/src/main/java/com/travelex/tgbp/schema/model/api/SchemaModelBuildException.java
+++ travelex/trunk/payment-gateway-schema-model/src/main/java/com/travelex/tgbp/schema/model/api/SchemaModelBuildException.java
@@ -2,7 +2,9 @@ package com.travelex.tgbp.schema.model.api;
 
 public class SchemaModelBuildException extends RuntimeException {
 
-    public SchemaModelBuildException(String arg0, Throwable arg1) {
+	private static final long serialVersionUID = 8796778298799686453L;
+
+	public SchemaModelBuildException(String arg0, Throwable arg1) {
         super(arg0, arg1);
     }
 
