62f4f657a53b42b1d605459108b110e548846ceb
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2010-02-06 16:55:37 +0000

    second tutorial

---
+++ sca4j/tutorials/tutorial-wiring/src/test/java/org/sca4j/tutorial/order/OrderITest.java
@@ -0,0 +1,34 @@
+/**
+ * SCA4J
+ * Copyright (c) 2009 - 2099 Service Symphony Ltd
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ */
+package org.sca4j.tutorial.order;
+
+import org.osoa.sca.annotations.Reference;
+import org.sca4j.tutorial.order.OrderService;
+
+import junit.framework.TestCase;
+
+public class OrderITest extends TestCase {
+    
+    @Reference protected OrderService orderService;
+
+    public void testPlaceOrder() {
+        assertTrue(orderService.placeOrder("Pizza", "70 Byron Rd", "70 Byron Road", "12345"));
+    }
+
+}
