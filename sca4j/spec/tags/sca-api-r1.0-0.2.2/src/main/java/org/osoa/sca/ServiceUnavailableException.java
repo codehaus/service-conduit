39e2f8a6360124ce63a008d4d8742b225430c388
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2010-03-20 11:54:35 +0000

    [maven-release-plugin]  copy for tag sca-api-r1.0-0.2.2

---
+++ sca4j/spec/tags/sca-api-r1.0-0.2.2/src/main/java/org/osoa/sca/ServiceUnavailableException.java
@@ -0,0 +1,117 @@
+/**
+ * SCA4J
+ * Copyright (c) 2009 - 2099 Service Symphony Ltd
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ *
+ *
+ * Original Codehaus Header
+ *
+ * Copyright (c) 2007 - 2008 fabric3 project contributors
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ *
+ * Original Apache Header
+ *
+ * Copyright (c) 2005 - 2006 The Apache Software Foundation
+ *
+ * Apache Tuscany is an effort undergoing incubation at The Apache Software
+ * Foundation (ASF), sponsored by the Apache Web Services PMC. Incubation is
+ * required of all newly accepted projects until a further review indicates that
+ * the infrastructure, communications, and decision making process have stabilized
+ * in a manner consistent with other successful ASF projects. While incubation
+ * status is not necessarily a reflection of the completeness or stability of the
+ * code, it does indicate that the project has yet to be fully endorsed by the ASF.
+ *
+ * This product includes software developed by
+ * The Apache Software Foundation (http://www.apache.org/).
+ */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+package org.osoa.sca;
+
+/**
+ * Exception used to indicate that a runtime exception occurred during the invocation of and external service.
+ *
+ * @version $Rev: 1 $ $Date: 2007-05-14 18:40:37 +0100 (Mon, 14 May 2007) $
+ */
+public class ServiceUnavailableException extends ServiceRuntimeException {
+
+    private static final long serialVersionUID = -5869397223249401047L;
+
+    /**
+     * Constructs a new ServiceUnavailableException.
+     */
+    public ServiceUnavailableException() {
+        super((Throwable) null);
+    }
+
+    /**
+     * Constructs a new ServiceUnavailableException with the specified detail message.
+     *
+     * @param message The detail message (which is saved to later retrieval by the getMessage() method).
+     */
+    public ServiceUnavailableException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new ServiceUnavailableException with the specified cause.
+     *
+     * @param cause The cause (which is saved to later retrieval by the getCause() method).
+     */
+    public ServiceUnavailableException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new ServiceUnavailableException with the specified detail message and cause.
+     *
+     * @param message The message (which is saved to later retrieval by the getMessage() method).
+     * @param cause   The cause (which is saved to later retrieval by the getCause() method).
+     */
+    public ServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ sca4j/spec/tags/sca-api-r1.0-0.2.2/src/main/java/org/osoa/sca/annotations/Intent.java
@@ -0,0 +1,93 @@
+/**
+ * SCA4J
+ * Copyright (c) 2009 - 2099 Service Symphony Ltd
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ *
+ *
+ * Original Codehaus Header
+ *
+ * Copyright (c) 2007 - 2008 fabric3 project contributors
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ *
+ * Original Apache Header
+ *
+ * Copyright (c) 2005 - 2006 The Apache Software Foundation
+ *
+ * Apache Tuscany is an effort undergoing incubation at The Apache Software
+ * Foundation (ASF), sponsored by the Apache Web Services PMC. Incubation is
+ * required of all newly accepted projects until a further review indicates that
+ * the infrastructure, communications, and decision making process have stabilized
+ * in a manner consistent with other successful ASF projects. While incubation
+ * status is not necessarily a reflection of the completeness or stability of the
+ * code, it does indicate that the project has yet to be fully endorsed by the ASF.
+ *
+ * This product includes software developed by
+ * The Apache Software Foundation (http://www.apache.org/).
+ */
+package org.osoa.sca.annotations;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation that can be applied to annotations that describe SCA intents.
+ * Adding this annotation allows SCA runtimes to automatically detect user-defined intents.
+ * <p/>
+ * Applications must specify a value, a pairing of targetNamespace and localPort, or both.
+ * If both value and pairing are supplied they must define the same qualified name.
+ *
+ * @version $Rev: 875 $ $Date: 2007-08-27 17:23:01 +0100 (Mon, 27 Aug 2007) $
+ */
+@Target({ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface Intent {
+    /**
+     * The qualified name of the intent, in the form defined by
+     * {@link javax.xml.namespace.QName#toString() QName.toString()}.
+     *
+     * @return the qualified name of the intent
+     */
+    String value() default "";
+
+    /**
+     * The XML namespace for the intent.
+     *
+     * @return the XML namespace for the intent
+     */
+    String targetNamespace() default "";
+
+    /**
+     * The name of the intent within its namespace.
+     *
+     * @return name of the intent within its namespace
+     */
+    String localPart() default "";
+}
