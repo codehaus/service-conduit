272589d48748dedcb0a4c40e57476d8b363e814d
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2010-04-22 17:57:26 +0000

    Starting on the embedded BPEL server

---
+++ sca4j/modules/trunk/extension/implementation/sca4j-bpel/src/main/java/org/sca4j/bpel/spi/EmbeddedBpelServer.java
@@ -0,0 +1,55 @@
+/**
+ * SCA4J
+ * Copyright (c) 2009 - 2099 Service Symphony Ltd
+ *
+ * Licensed to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the license
+ * is included in this distrubtion or you may obtain a copy at
+ *
+ *    http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * This project contains code licensed from the Apache Software Foundation under
+ * the Apache License, Version 2.0 and original code from project contributors.
+ */
+package org.sca4j.bpel.spi;
+
+import javax.xml.namespace.QName;
+
+import org.sca4j.bpel.provision.BpelPhysicalComponentDefinition;
+import org.sca4j.spi.wire.Interceptor;
+
+/**
+ * SPI for talking to an embedded BPEL server.
+ * 
+ * @author meerajk
+ *
+ */
+public interface EmbeddedBpelServer {
+    
+    /**
+     * @param procesName
+     * @param serviceName
+     * @param operation
+     * @param payload
+     * @return
+     */
+    Object invokeService(QName procesName,QName serviceName, String operation, Object payload);
+    
+    /**
+     * @param processName
+     * @param referenceName
+     * @param invoker
+     */
+    void addOutboundEndpoint(QName processName, QName referenceName, Interceptor invoker);
+    
+    /**
+     * @param physicalComponentDefinition
+     */
+    void registerProcess(BpelPhysicalComponentDefinition physicalComponentDefinition);
+
+}
