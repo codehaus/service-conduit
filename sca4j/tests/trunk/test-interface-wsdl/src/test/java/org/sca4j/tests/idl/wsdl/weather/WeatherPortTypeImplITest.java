5043177f76f488924dd761d904ba88c3e010b1cf
meerajk <meerajk@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2010-04-22 18:09:33 +0000

    Optimized imports

--- sca4j/tests/trunk/test-interface-wsdl/src/test/java/org/sca4j/tests/idl/wsdl/weather/WeatherPortTypeImplITest.java
+++ sca4j/tests/trunk/test-interface-wsdl/src/test/java/org/sca4j/tests/idl/wsdl/weather/WeatherPortTypeImplITest.java
@@ -18,9 +18,9 @@
  */
 package org.sca4j.tests.idl.wsdl.weather;
 
-import org.oasisopen.sca.annotation.Reference;
-
 import junit.framework.TestCase;
+
+import org.oasisopen.sca.annotation.Reference;
 
 public class WeatherPortTypeImplITest extends TestCase {
     
