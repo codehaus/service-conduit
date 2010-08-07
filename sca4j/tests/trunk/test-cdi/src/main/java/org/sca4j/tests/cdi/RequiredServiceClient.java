c975c925a223e5480b4bb886406e86a2db62e832
derekd <derekd@15bcc2b3-4398-4609-aa7c-97eea3cd106e>
2010-08-07 13:26:04 +0000

    Interim commit: Added qualifier annotation processing

---
+++ sca4j/tests/trunk/test-cdi/src/main/java/org/sca4j/tests/cdi/RequiredServiceClient.java
@@ -0,0 +1,12 @@
+package org.sca4j.tests.cdi;
+
+/**
+ * TODO: Check wiring targets with qualifiers
+ */
+public interface RequiredServiceClient {
+
+	String getFieldMessage();
+	String getMethodMessage();
+	String getConstructorMessage();
+	
+}
